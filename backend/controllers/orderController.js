121b94b9beec43dff6f369298112ceb49ba9e2b8
markgmz92 <markgmz92@gmail.com>
2021-01-19 10:03:59 +0800

    [PS - 043] Created order controller & route

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,40 @@
+import asyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
+
+// @desc Create new order
+// @route POST /api/orders
+// @access Private
+
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPirce,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No Order Items");
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPirce,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
+
+export { addOrderItems };
